71b1bc681117a8c9a4a4257b25f180cab3cb5fff
mariamdashti-c34 <70545845+mariamdashti-c34@users.noreply.github.com>
2020-10-03 17:09:13 +0300

    done

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
+
+<script>
+
+
+
 let instructor = {
   name: "Menawer",
   courses: ["JavaScript", "React", "PHP"],
   githubAccount: {
     username: "careless10",
     numberOfRepos: 200,
-  },
-};
+  };
+}
+
+</script>
+
